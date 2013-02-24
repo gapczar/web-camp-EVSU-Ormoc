d8eed1adc6f7b4d368f1a4511af0cc604747688a
GAPLabs <GAPLabs@czarpino-Mac-mini.local>
2013-02-24 14:43:24 +0800

    asfd

---
+++ web/main.php
@@ -0,0 +1,12 @@
+<?php
+	include('connect.php');
+?>
+<html>
+<head>
+</head>
+<body>
+<form type="input" method="post" action="login.php">
+	<a href="selectapps.php">Mobile apps</a>
+</form>
+</body>
+</html>
