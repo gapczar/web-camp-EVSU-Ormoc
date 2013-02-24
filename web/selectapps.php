e4c7e62b10fba68355f25e604cd46d608d620629
GAPLabs <GAPLabs@czarpino-Mac-mini.local>
2013-02-24 15:00:57 +0800

    asdf

--- web/selectapps.php
+++ web/selectapps.php
@@ -25,7 +25,7 @@
   <tr>
     <!--FIRST ROW -->
     <?php
-    $query="select * from mobile_app_info LIMIT 0, 5";
+    $query="select * from mobile_app_info";
     $result=mysql_query($query) or die("Error in query: $query ".mysql_error());
     $i=0;
     while($row=mysql_fetch_array($result)){
@@ -43,6 +43,7 @@
 	<input type="submit" name="vote" value="VOTE">
 <?php
 if(isset($_POST['vote'])){
+
 	foreach($_POST['app'] as $key => $value){
 		$gdg = mysql_query("select * from mobile_app_info where mob_id='$value'") or die (mysql_error());
 		while($dat=mysql_fetch_array($gdg)){
@@ -50,6 +51,13 @@ if(isset($_POST['vote'])){
 			$userid = $_SESSION['user_id'];
 			$fsdg = mysql_query("insert into vote(user_id,mob_id,vote_date) values('$userid','$mob_id',now())") or die (mysql_error());
 		}
+	}
+	views();
+}
+function views(){
+	$ggh = mysql_query("select * from vote A, mobile_app_info B where A.mob_id=B.mob_id ORDER By user_id DESC") or die (mysql_error());
+	while($data=mysql_fetch_array($ggh)){
+		echo '<img src="'.$data['mob_pic'].'" />';
 	}
 }
 ?>
