60523b2e1e28767b9de731dbd7650a6f80d890ad
GAPLabs <GAPLabs@www.noteify.org.local>
2013-02-24 09:31:23 +0800

    asdf

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+	include(connect.php);
+?>
+<html>
+<head>
+</head>
+<body>
+<form type="input" method="post" action="login.php">
+	<input type="text" name="username" />
+	<input type="password" name="password"/>
+	<input type="submit" name="login" value="Login">
+	<?php
+
+	?>
+</form>
+</body>
+</html>
