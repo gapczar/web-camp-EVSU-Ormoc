00e782adcb438efbc99fef7ac7ff854da1f77018
GAPLabs <GAPLabs@czarpino-Mac-mini.local>
2013-02-24 14:40:21 +0800

    asf

---
+++ web/connect.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+</head>
+<body>
+	<?php
+	$connection=mysql_connect('localhost','root','root') or die('Unable to connect!');
+	mysql_select_db('evsuocc') or die('Unable to connect database!');
+	?>
+</body>
+</html>
